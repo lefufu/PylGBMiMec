4c44257b682fe8bdd90640679f78851659e96993
lefufu <lefufu@users.noreply.github.com>
2020-09-20 20:38:23 +0200

    first public version

--- declarations/report_definitions.py
+++ declarations/report_definitions.py
@@ -1,4 +1,6 @@
-from basic_handling.error_handling import CriticalError, REPORT_NOT_DEFINED_FOR_TYPE
+from basic_functions.error_handling import criticalError, REPORT_NOT_DEFINED_FOR_TYPE
+
+""" definition of report name and ID to ease their use"""
 
 report_name=dict()
 report_name['Plane']={
@@ -25,29 +27,6 @@ report_name['Ship']={
     1:'OnTargetAttacked',
     2:'OnAreaAttacked'
 }
-report_name['MCU_TR_ComplexTrigger']={
-    57:'EventsFilterSpawned',
-    58:'EventsFilterEnteredSimple',
-    59:'EventsFilterEnteredAlive',
-    60:'EventsFilterLeftSimple',
-    61:'EventsFilterLeftAlive',
-    62:'EventsFilterFinishedSimple',
-    63:'EventsFilterFinishedAlive',
-    64:'EventsFilterStationaryAndAlive',
-    65:'EventsFilterFinishedStationaryAndAlive',
-    66:'EventsFilterTookOff',
-    67:'EventsFilterDamaged',
-    68:'EventsFilterCriticallyDamaged',
-    69:'EventsFilterRepaired',
-    70:'EventsFilterKilled',
-    71:'EventsFilterDropedBombs',
-    72:'EventsFilterFiredRockets',
-    73: 'EventsFilterFiredFlare',
-    75:'EventsFilterDroppedCargoContainers',
-    76:'EventsFilterDeliveredCargo',
-    77:'EventsFilterParatrooperJumped',
-    78:'EventsFilterParatrooperLandedAlive'
-}
 
 def findReport(typeObj:str, reportName:str):
     """ Find report number by entering object type and report name
@@ -59,7 +38,7 @@ def findReport(typeObj:str, reportName:str):
     foundNum=-1
 
     if typeObj not in report_name:
-        CriticalError(REPORT_NOT_DEFINED_FOR_TYPE.format(typeObj))
+        criticalError(REPORT_NOT_DEFINED_FOR_TYPE.format(typeObj))
 
     for reportNum in report_name[typeObj]:
         if report_name[typeObj][reportNum] == reportName:
