700a3ff3aef7869e5b578c6bb1f760c2d991151e
super-truite <super-truite@hotmail.fr>
2021-01-03 11:47:34 +0100

    added back setWindLayer in mission class

--- pylgbmimec/basic_functions/mission_class.py
+++ pylgbmimec/basic_functions/mission_class.py
@@ -1,8 +1,8 @@
 #from typing import List, Any
-from basic_functions.file_functions import getBegining, readGroupFromFile, readObjectFromFile
-from basic_functions.object_class import *
-from declarations.map_size import *
-from declarations.properties_specials import GUIMAP, OPTIONS, GROUP, XPOS, ZPOS
+from .file_functions import getBegining, readGroupFromFile, readObjectFromFile
+from .object_class import *
+from ..declarations.map_size import *
+from ..declarations.properties_specials import GUIMAP, OPTIONS, GROUP, XPOS, ZPOS
 
 class Mission:
     """Mission Class, contain an array of AllObject objects
@@ -145,7 +145,14 @@ class Mission:
         print(self.ObjList[objID])
         self.ObjList.pop(objID)
         return
-
+    # ---------------------------------------------
+    def setWindLayer(self, new_wind_layer_dictionary: dict):
+        for i, altitude in enumerate([0, 500, 1000, 2000, 5000]):
+            direction = new_wind_layer_dictionary[altitude]['direction']
+            speed = new_wind_layer_dictionary[altitude]['speed']
+            new_value = '    {0} :     {1} :     {2};\n'.format(altitude, direction, speed)
+            self.ObjList[0].PropList['WindLayers'][i].Value = new_value
+        return
 #---------------------------------------------
 def readMissionFromFile(mission:Mission, fileName: str) -> object:
         """ Read mission from IL2 GB .mission file"""
